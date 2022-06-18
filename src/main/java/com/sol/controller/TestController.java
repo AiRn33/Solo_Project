5ab3779f132b3fbb595b1285956536f992085855
SangBok Kim <tkdqhr6777@naver.com>
2022-06-18 14:35:36 +0900

    2 Day

--- src/main/java/com/sol/controller/TestController.java
+++ src/main/java/com/sol/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.sol.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
 
-	@GetMapping("/index")
-	public void index() {
+	@PostMapping("/test/one")
+	public void test(@RequestParam String name, @RequestParam String email, @RequestParam String phone) {
+		
+		System.out.println("test Code Post");
+		System.out.println("name : " + name);
+		System.out.println("email : " + email);
+		System.out.println("phone : " + phone);
+		
+	}
+	
+	@GetMapping("/test/one")
+	public void test1() {
+		System.out.println("test Code Get ");
 		
 	}
 }
